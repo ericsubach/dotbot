56eab407fb2ecc06b38abf604e1715f9dd4125c6
Eric Subach <eric.subach@indepth.com>
2014-10-03 21:59:10 +0000

    Add check for Windows support before import Windows libraries

--- dotbot/executor/__init__.py
+++ dotbot/executor/__init__.py
@@ -1,6 +1,13 @@
 from .executor import Executor
 from .linker import Linker
-from .linker_windows import LinkerWindows7
 from .cleaner import Cleaner
-from .cleaner_windows import CleanerWindows7
 from .commandrunner import CommandRunner
+
+# Windows 7 support.
+import os
+if os.name == 'nt':
+    try:
+        from .linker_windows import LinkerWindows7
+        from .cleaner_windows import CleanerWindows7
+    except ImportError: # TODO need correct error type for missing requirement
+        exit('The python library "win32file" must be installed for this program to work on Windows.')
